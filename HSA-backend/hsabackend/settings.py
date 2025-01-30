2e6756980d4a7a34decbd3aaabe722b9d33f4462
Alex Guo <aguo2@uiowa.edu>
2025-01-30 14:04:36 -0600

    HSA-5: added prod env, added gitignore to ignore virtual env

--- HSA-backend/hsabackend/settings.py
+++ HSA-backend/hsabackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--x=y15cdvxr)buip&^qxk=ndp@9m102@s2y#zr#gr)%mnfhgxy'
+if 'ENV' in os.environ and os.environ['ENV'] == 'PROD':
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if 'ENV' not in os.environ else False
 
 ALLOWED_HOSTS = []
 
