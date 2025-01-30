3e62fb527f5803500665d69a2b872864d95722c0
Stewart Sankey <stewart-sankey@uiowa.edu>
2025-01-29 22:41:27 -0600

    HSA-3: Added test component.

---
+++ HSA-frontend/src/app/components/test-component/test-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  imports: [],
+  templateUrl: './test-component.component.html',
+  styleUrl: './test-component.component.scss'
+})
+export class TestComponentComponent {
+
+}
