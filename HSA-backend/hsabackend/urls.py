9a99b8ca2edbc5a7ccc18251b56fb39384ec22ac
Alex Guo <aguo2@uiowa.edu>
2025-02-05 20:02:05 -0600

    fixed the routing

--- HSA-backend/hsabackend/urls.py
+++ HSA-backend/hsabackend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -27,7 +27,7 @@ urlpatterns = [
     # API
 
     # Standard routing
-    path('', hview.main_view)   
+    re_path(r'.*', hview.main_view)   
 ]
 
 if settings.DEBUG:
