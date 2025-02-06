964cfc6a6dd62f9d7b75213ed0d821cf91e8ec62
Alex Guo <aguo2@uiowa.edu>
2025-02-06 12:38:30 -0600

    HSA-25: fixed all failing tests

--- HSA-frontend/src/app/pages/login/login.component.spec.ts
+++ HSA-frontend/src/app/pages/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent],
+      providers: [provideAnimationsAsync()] 
     })
     .compileComponents();
 
