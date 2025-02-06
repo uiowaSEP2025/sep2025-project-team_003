875d025133ac969648d4b2018fbd7c4db86a87c2
Alex Guo <aguo2@uiowa.edu>
2025-02-06 13:52:52 -0600

    HSA-25: wip adding unit tests

--- HSA-frontend/src/app/pages/login/login.component.spec.ts
+++ HSA-frontend/src/app/pages/login/login.component.spec.ts
@@ -22,4 +22,44 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the correct input fields', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const formfields = compiled.querySelectorAll('mat-form-field')
+    expect(formfields[1].querySelector('mat-label').textContent).toContain('Password');
+    expect(formfields[0].querySelector('mat-label').textContent).toContain('Username');
+    
+    const buttonsArray:Element[] = Array.from(compiled.querySelectorAll('button'));
+    const loginButton = buttonsArray.filter((el:Element) => (el.textContent == 'Login'))[0]
+    const signUpButton = buttonsArray.filter((el:Element) => (el.textContent == 'Sign up'))[0]
+    expect(loginButton).toBeTruthy();
+    expect(signUpButton).toBeTruthy();
+  });
+
+  it('should be an invalid username when the username is empty', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const buttonsArray:Element[] = Array.from(compiled.querySelectorAll('button'));
+    const loginButton = buttonsArray.filter((el:Element) => (el.textContent == 'Login'))[0]
+    expect(loginButton).toBeTruthy();
+    (loginButton as HTMLElement).click();
+    
+    fixture.detectChanges(); // needed to detect the error field
+    const errorTextElement = compiled.querySelectorAll('mat-form-field')[0].querySelector('mat-error')
+    expect(errorTextElement.textContent).toEqual('Username is required');
+  });
+
+  it('should be an invalid password when the username is empty', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const buttonsArray:Element[] = Array.from(compiled.querySelectorAll('button'));
+    const loginButton = buttonsArray.filter((el:Element) => (el.textContent == 'Login'))[0]
+    expect(loginButton).toBeTruthy();
+    (loginButton as HTMLElement).click();
+    
+    fixture.detectChanges(); // needed to detect the error field
+    const errorTextElement = compiled.querySelectorAll('mat-form-field')[1].querySelector('mat-error')
+    expect(errorTextElement.textContent).toEqual('Password is required');
+  });
+
+
+
 });
