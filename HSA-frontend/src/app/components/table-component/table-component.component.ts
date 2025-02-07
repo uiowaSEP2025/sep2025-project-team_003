29fef68029df71e49f885eaff5ac5eb049a53400
Alex Guo <aguo2@uiowa.edu>
2025-02-07 14:46:00 -0600

    HSA-32: generated component

---
+++ HSA-frontend/src/app/components/table-component/table-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-component',
+  imports: [],
+  templateUrl: './table-component.component.html',
+  styleUrl: './table-component.component.scss'
+})
+export class TableComponentComponent {
+
+}
