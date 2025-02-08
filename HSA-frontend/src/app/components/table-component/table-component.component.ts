8a1a59786217bd98a2d8775900f6b9b11c2ccda5
Alex Guo <aguo2@uiowa.edu>
2025-02-07 23:48:52 -0600

    HSA-32-Frontend-added table

--- HSA-frontend/src/app/components/table-component/table-component.component.ts
+++ HSA-frontend/src/app/components/table-component/table-component.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,AfterViewInit,input } from '@angular/core';
+import { MatTableModule,MatTableDataSource } from '@angular/material/table';
+import { MatPaginator,MatPaginatorModule} from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
+// TODO: for data fetching, add a service: https://stackademic.com/blog/fetching-data-from-an-api-in-angular
 @Component({
   selector: 'app-table-component',
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, MatInputModule, MatSelectModule],
   templateUrl: './table-component.component.html',
   styleUrl: './table-component.component.scss'
 })
-export class TableComponentComponent {
-
+export class TableComponentComponent implements AfterViewInit {
+  headers = ['header1','header2','header3','header4']
+  data = new MatTableDataSource(rows) 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  searchField = input(0);
+  ngAfterViewInit() {
+    this.data.paginator = this.paginator;
+  }
 }
+ 
+const rows = [
+  {header1: 'cat',header2: 'dog', header3: 'fish', header4: 'snake'},
+  {header1: 'cat',header2: 'dog', header3: 'fish', header4: 'snake'},
+  {header1: 'cat',header2: 'dog', header3: 'fish', header4: 'snake'},
+  {header1: 'cat',header2: 'dog', header3: 'fish', header4: 'snake'},
+  {header1: 'cat',header2: 'dog', header3: 'fish', header4: 'snake'}
+]
--- HSA-frontend/src/app/pages/test-page/test-page.component.ts
+++ HSA-frontend/src/app/pages/test-page/test-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TableComponentComponent } from '../../components/table-component/table-component.component';
 
 @Component({
   selector: 'app-test-page',
-  imports: [],
+  imports: [TableComponentComponent],
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.scss'
 })
