be3b8a0259524dc4f0643472695505271eba4519
Alex Guo <aguo2@uiowa.edu>
2025-02-09 12:17:52 -0600

    HSA-32: wip errors not showing

--- HSA-frontend/src/app/components/table-component/table-component.component.ts
+++ HSA-frontend/src/app/components/table-component/table-component.component.ts
@@ -1,11 +1,36 @@
-import { Component, ViewChild, AfterViewInit, input } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, ValidatorFn, ValidationErrors,
+  FormGroup, AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { ReactiveFormsModule } from '@angular/forms';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export const formValidator: ValidatorFn = (
+  control: AbstractControl,
+): ValidationErrors | null => {
+  console.log('called')
+  const searchText = control.get('searchControl')?.value;
+  const select = control.get('selectControl')?.value;
+  if (searchText && !select) {
+    console.log("mustselect")
+    return {mustSelect: true}
+  }
+  if (!searchText && select) {
+    return {mustSearch: true}
+  }
+  return null
+};
 
 // TODO: for data fetching, add a service: https://stackademic.com/blog/fetching-data-from-an-api-in-angular
 @Component({
@@ -15,12 +40,16 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './table-component.component.scss'
 })
 export class TableComponentComponent implements AfterViewInit {
-  searchControl = new FormControl('');
-  selectControl = new FormControl('');
+  queryGroup = new FormGroup({
+    searchControl: new FormControl(''),
+    selectControl: new FormControl('', )
+  }, { validators: formValidator })
+
   page:number | null = null // null when unspecified
   pageSize:number | null = null // null when unspecified
   headers = ['header1', 'header2', 'header3', 'header4']
   data = new MatTableDataSource(rows)
+  matcher = new MyErrorStateMatcher();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   refetchData(searchTerm: string | null, searchHeader: string | null) {
@@ -31,33 +60,29 @@ export class TableComponentComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.data.paginator = this.paginator;
-    this.searchControl.valueChanges
+    this.queryGroup.controls.searchControl.valueChanges
       .pipe(
         debounceTime(300), // Wait for 300ms after the last change
         distinctUntilChanged() // Only emit if the value has changed
       )
-      .subscribe(searchTerm => {
-        this.refetchData(searchTerm,this.selectControl.value);
+      .subscribe((searchTerm) => {
+        this.refetchData(searchTerm,this.queryGroup.controls.selectControl.value);
       });
     
-    this.selectControl.valueChanges
+    this.queryGroup.controls.selectControl.valueChanges
       .pipe(
         debounceTime(300), 
         distinctUntilChanged()
       )
       .subscribe(headerName => {
-        this.refetchData(this.searchControl.value,headerName);
+        this.refetchData(this.queryGroup.controls.searchControl.value,headerName);
       });
 
       this.paginator.page.subscribe((page) => {
         this.page = page.pageIndex;
         this.pageSize = page.pageSize;
-        this.refetchData(this.searchControl.value,this.selectControl.value);
-      })
-
-      
-
-  }
+        this.refetchData(this.queryGroup.controls.searchControl.value,this.queryGroup.controls.selectControl.value);
+      })}
 }
 
 const rows = [
