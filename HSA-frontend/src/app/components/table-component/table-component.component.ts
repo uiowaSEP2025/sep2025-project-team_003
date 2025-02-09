1d2fd74ac1e37a165f7ce72eaa0ed084951ec4ca
Alex Guo <aguo2@uiowa.edu>
2025-02-09 13:40:59 -0600

    HSA-32: finally figured out the stupid error state matchetr

--- HSA-frontend/src/app/components/table-component/table-component.component.ts
+++ HSA-frontend/src/app/components/table-component/table-component.component.ts
@@ -11,8 +11,7 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 /** Error when invalid control is dirty, touched, or submitted. */
 class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(control?.parent && control.parent.invalid && (control.dirty || control.touched));
   }
 }
 
