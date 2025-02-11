dd5899d71cf0464d8f4480007fc01770c7fb3485
Alex Guo <aguo2@uiowa.edu>
2025-02-11 12:19:25 -0600

    added page component

---
+++ HSA-frontend/src/app/pages/customers-page/customers-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-page',
+  imports: [],
+  templateUrl: './customers-page.component.html',
+  styleUrl: './customers-page.component.scss'
+})
+export class CustomersPageComponent {
+
+}
