d4f54ecac8d82f441c6bdaa2a99ab1f57f9ac8c5
Alex Guo <aguo2@uiowa.edu>
2025-02-13 11:33:22 -0600

    HSA-29: pulled out form error state matcher out

--- HSA-frontend/src/app/pages/login/login.component.ts
+++ HSA-frontend/src/app/pages/login/login.component.ts
@@ -2,23 +2,13 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import {ErrorStateMatcher} from '@angular/material/core';
 import {
   FormControl,
-  FormGroupDirective,
-  NgForm,
   Validators,
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
-
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+import { GenericFormErrorStateMatcher } from '../../utils/generic-form-error-state-matcher';
 
 @Component({
   selector: 'app-login',
@@ -27,7 +17,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  matcher = new MyErrorStateMatcher();
+  matcher = new GenericFormErrorStateMatcher();
   usernameFormControl = new FormControl('', [Validators.required]);
   passwordFormControl = new FormControl('', [Validators.required]);
 }
