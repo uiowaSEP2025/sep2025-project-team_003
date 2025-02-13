4b8956dd8633d1226d1240753c46d30f20c3663d
Alex Guo <aguo2@uiowa.edu>
2025-02-13 12:16:18 -0600

    HSA-29 the phone component still dosne't work

--- HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
+++ HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { PhoneNumberInputComponent } from '../../components/phone-number-input/phone-number-input-component';
-import { FormControl } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl,ReactiveFormsModule,Validators } from '@angular/forms';
 import { GenericFormErrorStateMatcher } from '../../utils/generic-form-error-state-matcher';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-customer-page',
-  imports: [MatInputModule,PhoneNumberInputComponent, ReactiveFormsModule],
+  imports: [FormsModule, MatInputModule, PhoneNumberInputComponent, ReactiveFormsModule, MatButtonModule],
   templateUrl: './create-customer-page.component.html',
   styleUrl: './create-customer-page.component.scss'
 })
 export class CreateCustomerPageComponent {
-  firstNameControl = new FormControl('')
+  firstNameControl = new FormControl('', Validators.required)
+  lastNameControl = new FormControl('', Validators.required)
+  emailControl = new FormControl('', [Validators.email, Validators.required])
+  phoneControl = new FormControl('')
+  notesControl = new FormControl('')
   matcher = new GenericFormErrorStateMatcher()
 }
