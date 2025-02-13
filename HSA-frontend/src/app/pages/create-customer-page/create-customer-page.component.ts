aef4d62555257ec50259d61c52d7dde92b46ec45
Alex Guo <aguo2@uiowa.edu>
2025-02-13 17:21:05 -0600

    HSA-29: gave up on the phone input

--- HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
+++ HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
@@ -1,26 +1,24 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
-import { InternalInputManager } from '../../components/phone-number-input/phone-number-input-component';
 import { FormControl,ReactiveFormsModule,Validators } from '@angular/forms';
 import { GenericFormErrorStateMatcher } from '../../utils/generic-form-error-state-matcher';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-import { MyTel } from '../../components/phone-number-input/phone-number-input-component';
 import { MatIconModule } from '@angular/material/icon';
+import { ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-create-customer-page',
-  imports: [FormsModule, MatInputModule, InternalInputManager, ReactiveFormsModule, MatButtonModule, MatIconModule],
+  imports: [FormsModule, MatInputModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
   templateUrl: './create-customer-page.component.html',
   styleUrl: './create-customer-page.component.scss'
 })
 export class CreateCustomerPageComponent {
+  @ViewChild('telInputContainer') phoneInput!: HTMLElement;
   firstNameControl = new FormControl('', Validators.required)
   lastNameControl = new FormControl('', Validators.required)
   emailControl = new FormControl('', [Validators.email, Validators.required])
-  phoneControl = new FormControl(new MyTel('', '', ''))
+  phoneControl = new FormControl('', Validators.required)
   notesControl = new FormControl('')
   matcher = new GenericFormErrorStateMatcher()
-  cl() {
-    console.log(this.phoneControl)
-  }
 }
--- HSA-frontend/src/app/pages/test-page/test-page.component.ts
+++ HSA-frontend/src/app/pages/test-page/test-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { InternalInputManager } from '../../components/phone-number-input/phone-number-input-component';
 
 @Component({
   selector: 'app-test-page',
-  imports: [InternalInputManager],
+  imports: [],
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.scss'
 })
