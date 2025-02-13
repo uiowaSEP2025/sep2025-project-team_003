530afd59cc4fa579821bf92009489afe864a2def
Alex Guo <aguo2@uiowa.edu>
2025-02-13 11:40:10 -0600

    HSA-29: it jsut dissapears like fucjing magic

--- HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
+++ HSA-frontend/src/app/pages/create-customer-page/create-customer-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { PhoneNumberInputComponent } from '../../components/phone-number-input/phone-number-input-component';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GenericFormErrorStateMatcher } from '../../utils/generic-form-error-state-matcher';
+
 @Component({
   selector: 'app-create-customer-page',
-  imports: [MatInputModule,],
+  imports: [MatInputModule,PhoneNumberInputComponent, ReactiveFormsModule],
   templateUrl: './create-customer-page.component.html',
   styleUrl: './create-customer-page.component.scss'
 })
 export class CreateCustomerPageComponent {
-
+  firstNameControl = new FormControl('')
+  matcher = new GenericFormErrorStateMatcher()
 }
