7ee193f48e9d002ea4e6e20b2823c00612cc3230
Alex Guo <aguo2@uiowa.edu>
2025-02-26 21:18:40 -0600

    HSA-52: done

--- HSA-frontend/src/app/layout/header/header.component.ts
+++ HSA-frontend/src/app/layout/header/header.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbar,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    CommonModule  
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  sidebarExpanded = false;
 
+  toggleSidebar() {
+    this.sidebarExpanded = !this.sidebarExpanded
+  }
 }
