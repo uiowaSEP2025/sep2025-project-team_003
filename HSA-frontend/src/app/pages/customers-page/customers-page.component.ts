899d8bab7d71caddc6ca7434a2351d642f90b56b
Alex Guo <aguo2@uiowa.edu>
2025-02-27 17:08:07 -0600

    wip

--- HSA-frontend/src/app/pages/customers-page/customers-page.component.ts
+++ HSA-frontend/src/app/pages/customers-page/customers-page.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { CustomerService } from '../../services/customer.service';
+import { TableApiResponse } from '../../interfaces/table.api.interface';
+import { Customer } from '../../interfaces/customer.interface';
 
 
 @Component({
@@ -14,9 +16,25 @@ import { CustomerService } from '../../services/customer.service';
 })
 export class CustomersPageComponent {
   customerService: CustomerService
+  custommer: TableApiResponse<Customer>
 
   constructor(private router: Router, customerService: CustomerService) {
     this.customerService = customerService
+  }
+
+  loadDataToTable(searchTerm: string, pageSize: number, offSet: number) {
+    this.customerService.getCustomer({ search: searchTerm, pagesize: pageSize, offset: offSet}).subscribe({
+      next: (response) => {
+        this.materials = response
+      },
+      error: (error) => {
+        
+      }
+    })
+  }
+
+  onPageChange(event: { searchTerm: string; pageSize: number; offset: number }): void {
+    this.loadDataToTable(event.searchTerm, event.pageSize, event.offset);
   }
 
   navigateToPage(pagePath: string) {
