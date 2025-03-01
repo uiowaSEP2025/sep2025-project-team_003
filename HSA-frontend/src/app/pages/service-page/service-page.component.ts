52718a86507625c49f8a1538c72f312d5b6714ef
Alex Guo <aguo2@uiowa.edu>
2025-03-01 12:31:42 -0600

    HSA-61: added services crud

--- HSA-frontend/src/app/pages/service-page/service-page.component.ts
+++ HSA-frontend/src/app/pages/service-page/service-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatFabButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {TableComponentComponent} from '../../components/table-component/table-component.component';
@@ -15,12 +15,16 @@ import { ServiceService } from '../../services/service.service';
   templateUrl: './service-page.component.html',
   styleUrl: './service-page.component.scss'
 })
-export class ServicePageComponent {
+export class ServicePageComponent implements OnInit {
   services: any
   serviceService: ServiceService
   
   constructor(private router: Router, serviceService: ServiceService) {
     this.serviceService = serviceService
+  }
+
+  ngOnInit(): void {
+    this.loadDataToTable("", 5, 0);
   }
 
   loadDataToTable(searchTerm: string, pageSize: number, offSet: number) {
@@ -29,7 +33,9 @@ export class ServicePageComponent {
         this.services = response
       },
       error: (error) => {
-        
+        if (error.status === 401) {
+          this.router.navigate(['/login']);
+        }
       }
     })
   }
