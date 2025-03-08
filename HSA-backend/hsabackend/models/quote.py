febb10411aad26b842e5c6cd7db081edd244774d
Alex Guo <aguo2@uiowa.edu>
2025-03-08 15:01:21 -0600

    HSA-42: tied quote to invoice

--- HSA-backend/hsabackend/models/quote.py
+++ HSA-backend/hsabackend/models/quote.py
@@ -1,6 +1,7 @@
 from django.db import models
 from hsabackend.models.discount_type import DiscountType
 from hsabackend.models.job import Job
+from hsabackend.models.invoice import Invoice
 
 class Quote(models.Model):
     """The price for a job that the organization sends to the customer for approval"""
@@ -16,6 +17,7 @@ class Quote(models.Model):
     total_price = models.FloatField()
     jobID = models.OneToOneField(Job, on_delete= models.CASCADE)
     discount_type = models.ForeignKey(DiscountType, on_delete=models.CASCADE)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return f"<Quote, job: {self.jobID}>"
