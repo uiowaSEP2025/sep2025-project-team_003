55469a2b3e5af5407890c9112f4fa379d8089698
Mike Zeng <mzen17.jp@gmail.com>
2025-03-12 00:04:34 +0000

    HSA-18/IMPL fix full script

--- k8-push.sh
+++ k8-push.sh
@@ -5,20 +5,20 @@
 # 3. Runs the terraform to update k8s
 # Build Docker image
 
-#docker build -t mzeng1417/hsa-image-store:latest .
+docker build -t mzeng1417/hsa-image-store:latest .
 
 # Transfer Docker image to remote host
-#docker push mzeng1417/hsa-image-store:latest
+docker push mzeng1417/hsa-image-store:latest
 
 kubectl delete service hsa-app-service
-#kubectl delete pods hsa-app
+kubectl delete pods hsa-app
 
 # adjust the YAML
-#sed -e "s|\${DATABASE_NAME}|${DATABASE_NAME}|g" \
-   # -e "s|\${DATABASE_USERNAME}|${DATABASE_USERNAME}|g" \
-   # -e "s|\${DATABASE_IP}|${DATABASE_IP}|g" \
- #   -e "s|\${DATABASE_PASSWORD}|${DATABASE_PASSWORD}|g" \
-  ##  kubernates/hsa-app-template.yaml > terraform.tf.yaml
+sed -e "s|\${DATABASE_NAME}|${DATABASE_NAME}|g" \
+    -e "s|\${DATABASE_USERNAME}|${DATABASE_USERNAME}|g" \
+    -e "s|\${DATABASE_IP}|${DATABASE_IP}|g" \
+    -e "s|\${DATABASE_PASSWORD}|${DATABASE_PASSWORD}|g" \
+    kubernates/hsa-app-template.yaml > terraform.tf.yaml
 
 #kubectl apply -f terraform.tf.yaml
 kubectl apply -f kubernates/hsa-app-service.yaml
