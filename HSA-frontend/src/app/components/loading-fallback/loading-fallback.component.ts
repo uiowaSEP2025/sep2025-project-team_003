636be30142d8ed09bf238bee485510c77c353aa9
Alex Guo <aguo2@uiowa.edu>
2025-03-13 13:56:31 -0500

    HSA-68: added loading fallback component template

---
+++ HSA-frontend/src/app/components/loading-fallback/loading-fallback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-fallback',
+  imports: [],
+  templateUrl: './loading-fallback.component.html',
+  styleUrl: './loading-fallback.component.scss'
+})
+export class LoadingFallbackComponent {
+
+}
