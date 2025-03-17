9ec4dada7f9a601c57b5a4f6e54d71431b77864c
Alex Guo <aguo2@uiowa.edu>
2025-03-17 12:45:57 -0400

    HSA-68: added stubs for create invoice

--- HSA-frontend/src/app/pages/create-invoice-page/create-invoice-page.component.spec.ts
+++ HSA-frontend/src/app/pages/create-invoice-page/create-invoice-page.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateInvoicePageComponent } from './create-invoice-page.component';
 
-describe('CreateInvoicePageComponent', () => {
+fdescribe('CreateInvoicePageComponent', () => {
   let component: CreateInvoicePageComponent;
   let fixture: ComponentFixture<CreateInvoicePageComponent>;
 
@@ -21,10 +21,14 @@ describe('CreateInvoicePageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should contain the correct elements', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    const table = compiled.querySelector('table');
-    expect(table).toBeTruthy()
-    
-  }) 
+  it('should display error when no customer is selected', () => {})
+
+  it('should display date picker when status is not created', () => {})
+
+  it('should call validate on the view child', () => {})
+
+  it('should show error when no quotes are selected', () => {})
+
+  it('should call the endpoint when everything is valid', () => {})
+
 });
