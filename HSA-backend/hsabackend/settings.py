5121623261f9b3509ce7c56c1f675741ab2145b8
Stewart Sankey <stewart-sankey@uiowa.edu>
2025-03-24 18:22:37 -0500

    testing django deployment issues to dev

--- HSA-backend/hsabackend/settings.py
+++ HSA-backend/hsabackend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +54,61 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+# Configure CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://198.178.169.192.host.secureserver.net",
+
+    "https://198.178.169.192.host.secureserver.net",
+
+    "http://192.169.178.198:8080",
+
+]
+
+
+CORS_ALLOW_METHODS = [
+
+    'DELETE',
+
+    'GET',
+
+    'OPTIONS',
+
+    'PATCH',
+
+    'POST',
+
+    'PUT',
+
+]
+
+
+CORS_ALLOW_HEADERS = [
+
+    'accept',
+
+    'accept-encoding',
+
+    'authorization',
+
+    'content-type',
+
+    'dnt',
+
+    'origin',
+
+    'user-agent',
+
+    'x-csrftoken',
+
+    'x-requested-with',
+
+]
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
