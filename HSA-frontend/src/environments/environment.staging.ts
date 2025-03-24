78fa365ac0dd90c874481cf150f304c559c82c72
Stewart Sankey <stewart-sankey@uiowa.edu>
2025-03-24 12:41:47 -0500

    updated docker and testing deployment

--- HSA-frontend/src/environments/environment.staging.ts
+++ HSA-frontend/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 import { EnviornmentInterface } from "../app/interfaces/enviornment.interface";
 
 export const environment:EnviornmentInterface = {
-    apiUrl: "192.169.178.198:4200",
+    apiUrl: "192.169.178.198:5432",
     requireCSRF: true
 };
