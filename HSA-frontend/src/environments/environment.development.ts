3b3b2f92f083ecc079e95c7b78ab71245316a72a
Stewart Sankey <stewart-sankey@uiowa.edu>
2025-03-24 18:59:43 -0500

    testing django deployment issues

--- HSA-frontend/src/environments/environment.development.ts
+++ HSA-frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 import { EnviornmentInterface } from "../app/interfaces/enviornment.interface";
 export const environment:EnviornmentInterface = {
 
-    apiUrl: "http://198.178.169.192.host.secureserver.net:8080",
+    apiUrl: "http://localhost:8000",
     requireCSRF: false
 }
