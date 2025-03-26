79498876847185a629ff069376ae6e43682e7747
Alex Guo <aguo2@uiowa.edu>
2025-03-26 16:49:32 -0500

    HSA-58: added chrome driver

--- HSA-backend/hsabackend/features/environment.py
+++ HSA-backend/hsabackend/features/environment.py
@@ -1,23 +1,52 @@
 import subprocess
 import time
 import os
+import requests
+from requests.exceptions import ConnectionError
+import time
+from selenium import webdriver
+
+
+def block_until_server_is_up(url, timeout=30, interval=2):
+    """Block untill the server is up."""
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            response = requests.get(url)
+            if response.ok:
+                print("angular was started successfully!!")
+                return True
+        except ConnectionError:
+            pass
+        time.sleep(interval)
+    return False
 
 def before_all(context):
-    """Start the Angular app before tests run."""
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../HSA-frontend')) # frontend to do ng serve
-    os.chdir(path)
-    context.server_process = subprocess.Popen(["ng", "serve", "--port", "4200"])
-    time.sleep(5)  # Give it time to start
+    """If runing on CICD ```behave -D CI=True```"""
+    context.is_CI = context.config.userdata.get("CI", False)
+    context.browser = webdriver.Chrome()
+
     
-    if context.server_process.poll() is not None:
-        error_message = "Angular server failed to start. \n"
-        if context.server_process.returncode == 0:
-            error_message += "Process exited successfully but shouldn't have.\n"
-        else:
-            error_message += f"Process exited with return code {context.server_process.returncode} \n"
-        raise RuntimeError(error_message)
+    if context.is_CI:
+        pass
+    else:
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../HSA-frontend')) # frontend to do ng serve
+        os.chdir(path)
+        context.angular_process = subprocess.Popen(["ng", "serve", "--port", "4200"])
+        block_until_server_is_up("http://localhost:4200")
+        
+        if context.angular_process.poll() is not None:
+            error_message = "Angular server failed to start. \n"
+            if context.server_process.returncode == 0:
+                error_message += "Process exited successfully but shouldn't have.\n"
+            else:
+                error_message += f"Process exited with return code {context.server_process.returncode} \n"
+            raise RuntimeError(error_message)
 
 
 def after_all(context):
-    """Stop the Angular app after tests complete."""
-    context.server_process.terminate()
+    if context.is_CI:
+        pass
+    else:
+        context.angular_process.terminate()
+        context.browser.quit()
