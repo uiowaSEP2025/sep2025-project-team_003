c41fc3fcb5d3cb35c44d25cc87d45f00bff97d6f
ssankey <ssankey@uiowa.edu>
2025-03-31 00:06:09 -0500

    HSA-84: troubleshooting deployment

---
+++ deployment/testing/integration/install-chrome.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Fetch the latest ChromeDriver version
+CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+echo "Latest ChromeDriver version: $CHROMEDRIVER_VERSION"
+
+# Extract Chrome major version from ChromeDriver version
+CHROME_MAJOR_VERSION=$(echo $CHROMEDRIVER_VERSION | cut -d. -f1)
+
+# Download specific Chrome version
+wget -q https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_MAJOR_VERSION}.0.6998.0-1_amd64.deb -O /tmp/chrome.deb
+
+# Remove existing Chrome
+apt-get remove -y google-chrome-stable
+
+# Install the specific Chrome version
+dpkg -i /tmp/chrome.deb || apt-get install -y -f
+
+# Clean up
+rm /tmp/chrome.deb
+
+# Verify installation
+google-chrome --version
--- deployment/testing/integration/install-chromedriver.sh
+++ deployment/testing/integration/install-chromedriver.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
 set -e
 
-# Get Chrome version
-CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
-echo "Detected Chrome version: $CHROME_VERSION"
-
-# Function to get the latest compatible ChromeDriver
-get_chromedriver_version() {
-    local major_version=$1
-
-    # Try descending versions until we find a compatible ChromeDriver
-    while [ $major_version -gt 0 ]; do
-        echo "Trying to find ChromeDriver for version $major_version" >&2
-
-        # Attempt to get the latest release for this version
-        DRIVER_VERSION=$(curl -sS "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${major_version}" 2>/dev/null)
-
-        # Check if we got a valid version
-        if [[ "$DRIVER_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-            echo "$DRIVER_VERSION"
-            return 0
-        fi
-
-        # Decrement version and try again
-        major_version=$((major_version - 1))
-    done
-
-    # If all else fails, get the latest overall version
-    curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE
-}
-
-# Get the appropriate ChromeDriver version
-CHROME_DRIVER_VERSION=$(get_chromedriver_version $CHROME_VERSION)
-echo "Using ChromeDriver version: $CHROME_DRIVER_VERSION"
+# Fetch the latest ChromeDriver version
+CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+echo "Installing ChromeDriver version: $CHROMEDRIVER_VERSION"
 
 # Download ChromeDriver
-wget -q "https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip" -O /tmp/chromedriver.zip
+wget -q "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip" -O /tmp/chromedriver.zip
 
 # Unzip and install
 unzip /tmp/chromedriver.zip -d /usr/local/bin/
