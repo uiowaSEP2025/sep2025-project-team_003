076eb2dd2c828cfc640ccb0a9d4e8aed796df22f
Alex Guo <aguo2@uiowa.edu>
2025-04-06 20:26:34 -0500

    HSA-119: working on tests

--- HSA-backend/hsabackend/tests/test_discounts.py
+++ HSA-backend/hsabackend/tests/test_discounts.py
@@ -6,10 +6,11 @@ from unittest.mock import patch
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from hsabackend.models.organization import Organization
+from django.core.exceptions import ValidationError
 from hsabackend.views.discounts import get_discounts, create_discount, edit_discount, delete_discount
 
 class DiscountTableTest(APITestCase):
-    def test_get_customer_table_data_unauth(self):
+    def test_get_discount_table_data_unauth(self):
         mock_user = Mock(spec=User)
         mock_user.is_authenticated = False
         
@@ -21,7 +22,7 @@ class DiscountTableTest(APITestCase):
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     @patch('hsabackend.views.discounts.Organization.objects.get')
-    def test_get_customer_table_data_missing_data(self, org):
+    def test_get_discount_table_data_missing_data(self, org):
         mock_user = Mock(spec=User)
         org.return_value = Organization()
         mock_user.is_authenticated = True
@@ -34,7 +35,7 @@ class DiscountTableTest(APITestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     @patch('hsabackend.views.discounts.Organization.objects.get')
-    def test_get_customer_table_data_bad_data(self,org):
+    def test_get_discount_table_data_bad_data(self,org):
         mock_user = Mock(spec=User)
         mock_user.is_authenticated = True
         org.return_value = Organization()
@@ -48,7 +49,7 @@ class DiscountTableTest(APITestCase):
 
     @patch('hsabackend.views.discounts.DiscountType.objects.filter')
     @patch('hsabackend.views.discounts.Organization.objects.get')
-    def test_get_customer_table_data_bad_data(self,org,discounts):
+    def test_get_discount_table_data_bad_data(self,org,discounts):
         mock_user = Mock(spec=User)
         mock_user.is_authenticated = True
         qs = MagicMock()
@@ -66,7 +67,57 @@ class DiscountTableTest(APITestCase):
 
 
 class CreateDiscountTest(APITestCase):
-    pass
+    def test_create_disount_unauth(self):
+        mock_user = Mock(spec=User)
+        mock_user.is_authenticated = False
+        
+        factory = APIRequestFactory()
+        request = factory.post('/api/create/discount')
+        request.user = mock_user  
+        response = create_discount(request)
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    @patch('hsabackend.views.discounts.Organization.objects.get')
+    @patch('hsabackend.views.discounts.DiscountType')
+    def test_create_disount_validation_failed(self,discnt, org):
+        org.return_value = Organization()
+        mock_discnt = Mock()
+        mock_discnt.full_clean.side_effect = ValidationError({'name': ['This field is required.']})
+        discnt.return_value = mock_discnt
+        
+        mock_user = Mock(spec=User)
+        mock_user.is_authenticated = True
+        
+        factory = APIRequestFactory()
+        request = factory.post('/api/create/discount')
+        request.user = mock_user  
+        response = create_discount(request)
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    @patch('hsabackend.views.discounts.Organization.objects.get')
+    @patch('hsabackend.views.discounts.DiscountType')
+    def test_create_disount_create_ok(self,discnt, org):
+        org.return_value = Organization()
+        mock_discnt = Mock()
+
+        mock_user = Mock(spec=User)
+        mock_user.is_authenticated = True
+        discnt.return_value = mock_discnt
+        
+        factory = APIRequestFactory()
+        request = factory.post('/api/create/discount', data={
+            "name": "starwars",
+            "percent":  "22.0"
+        })
+        request.user = mock_user  
+        response = create_discount(request)
+        
+        mock_discnt.save.assert_called_once()
+        assert response.status_code == status.HTTP_201_CREATED
+
+    
 
 class EditDiscountTest(APITestCase):
     pass
