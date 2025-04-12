b09b20d4918a2ab903b15ed9704b484595563c60
Alex Guo <aguo2@uiowa.edu>
2025-04-12 16:06:22 -0500

    HSA-99: added more frontend tests

--- HSA-frontend/src/app/pages/password-reset-page/password-reset-page.component.spec.ts
+++ HSA-frontend/src/app/pages/password-reset-page/password-reset-page.component.spec.ts
@@ -9,6 +9,7 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
 
 
 fdescribe('PasswordResetPageComponent', () => {
@@ -62,10 +63,10 @@ fdescribe('PasswordResetPageComponent', () => {
         return text === 'Confirm Password' ? input : null;
       })
     ).then(results => results.filter(input => input !== null)); // correctly working async filter
-    
+
     expect(title.textContent).toContain('Reset Password')
-    expect(passInput).toBeTruthy()
-    expect(passConfirmInput).toBeTruthy()
+    expect(passInput.length).toBeGreaterThan(0)
+    expect(passConfirmInput.length).toBeGreaterThan(0)
     expect(submitButton).toBeTruthy()
   })
 
@@ -73,39 +74,173 @@ fdescribe('PasswordResetPageComponent', () => {
     const submitButton = await loader.getHarness(MatButtonHarness)
     await submitButton.click()
     const compiled = fixture.debugElement.nativeElement;
-    
+
     const errors = Array.from(compiled.querySelectorAll('mat-error'));
 
     const requiredFirstPassError = (errors as HTMLElement[]).filter(
       (error) => error.textContent === "Password is required"
     );
 
-    expect(requiredFirstPassError[0]).toBeTruthy()    
+    expect(requiredFirstPassError[0]).toBeTruthy()
 
   })
 
-  it('should require the second password', async () => { 
-      const submitButton = await loader.getHarness(MatButtonHarness)
-      await submitButton.click()
-      const compiled = fixture.debugElement.nativeElement;
-      
-      const errors = Array.from(compiled.querySelectorAll('mat-error'));
-  
-      const requiredFirstPassError = (errors as HTMLElement[]).filter(
-        (error) => error.textContent === "Password is required"
-      );
-  
-      expect(requiredFirstPassError[1]).toBeTruthy()    
-  
+  it('should require the second password', async () => {
+    const submitButton = await loader.getHarness(MatButtonHarness)
+    await submitButton.click()
+    const compiled = fixture.debugElement.nativeElement;
+
+    const errors = Array.from(compiled.querySelectorAll('mat-error'));
+
+    const requiredFirstPassError = (errors as HTMLElement[]).filter(
+      (error) => error.textContent === "Password is required"
+    );
+    expect(requiredFirstPassError[1]).toBeTruthy()
+
   })
 
   describe('password strength checks', () => {
 
-    it('should require a number', () => { })
+    it('should require a number', async () => {
+      const submitButton = await loader.getHarness(MatButtonHarness)
+      const compiled = fixture.debugElement.nativeElement;
+      const passInput = await Promise.all(
+        (await loader.getAllHarnesses(MatFormFieldHarness)).map(async (input) => {
+          const text = await input.getLabel();
+          return text === 'Password' ? input : null;
+        })
+      ).then(results => results.filter(input => input !== null)); // correctly working async filter
 
-    it('should require a special character', () => { })
+      const control = await passInput[0].getControl() as unknown as MatInputHarness;
+      
+      await control.setValue('aaaaaaaaA@')
 
-    it('should require an uppercase letter', () => { })
+      await submitButton.click()
+      fixture.detectChanges()
+
+      const errors = Array.from(compiled.querySelectorAll('mat-error'));
+      const numberRequiredError = (errors as HTMLElement[]).filter(
+        (error) => {
+          return error.textContent!.includes("Must have 1 number")
+        }
+      );
+      
+      expect(numberRequiredError[0]).toBeTruthy()
+    })
+
+    it('should require a special character', async() => {
+      const submitButton = await loader.getHarness(MatButtonHarness)
+      const compiled = fixture.debugElement.nativeElement;
+      const passInput = await Promise.all(
+        (await loader.getAllHarnesses(MatFormFieldHarness)).map(async (input) => {
+          const text = await input.getLabel();
+          return text === 'Password' ? input : null;
+        })
+      ).then(results => results.filter(input => input !== null)); // correctly working async filter
+
+      const control = await passInput[0].getControl() as unknown as MatInputHarness;
+      
+      await control.setValue('aaaaaaaaA1')
+
+      await submitButton.click()
+      fixture.detectChanges()
+
+      const errors = Array.from(compiled.querySelectorAll('mat-error'));
+      const specialCharRequiredError = (errors as HTMLElement[]).filter(
+        (error) => {
+          return error.textContent!.includes("Must have 1 special character")
+        }
+      );
+      
+      expect(specialCharRequiredError[0]).toBeTruthy()
+    })
+
+    it('should require an uppercase letter', async () => {
+      const submitButton = await loader.getHarness(MatButtonHarness)
+      const compiled = fixture.debugElement.nativeElement;
+      const passInput = await Promise.all(
+        (await loader.getAllHarnesses(MatFormFieldHarness)).map(async (input) => {
+          const text = await input.getLabel();
+          return text === 'Password' ? input : null;
+        })
+      ).then(results => results.filter(input => input !== null)); // correctly working async filter
+
+      const control = await passInput[0].getControl() as unknown as MatInputHarness;
+      
+      await control.setValue('aaaaaaaa@1')
+
+      await submitButton.click()
+      fixture.detectChanges()
+
+      const errors = Array.from(compiled.querySelectorAll('mat-error'));
+      const uppercaseRequiredError = (errors as HTMLElement[]).filter(
+        (error) => {
+          return error.textContent!.includes("Must have 1 uppercase letter")
+        }
+      );
+      
+      expect(uppercaseRequiredError[0]).toBeTruthy()
+    })
+  })
+
+  describe('password length checks', () => {
+
+    it('should require length greater than 8', async () => {
+      const submitButton = await loader.getHarness(MatButtonHarness)
+      const compiled = fixture.debugElement.nativeElement;
+      const passInput = await Promise.all(
+        (await loader.getAllHarnesses(MatFormFieldHarness)).map(async (input) => {
+          const text = await input.getLabel();
+          return text === 'Password' ? input : null;
+        })
+      ).then(results => results.filter(input => input !== null)); // correctly working async filter
+
+      const control = await passInput[0].getControl() as unknown as MatInputHarness;
+      
+      await control.setValue('a')
+
+      await submitButton.click()
+      fixture.detectChanges()
+
+      const errors = Array.from(compiled.querySelectorAll('mat-error'));
+      const tooShortError = (errors as HTMLElement[]).filter(
+        (error) => {
+          return error.textContent!.includes("Minimum 8 characters required")
+        }
+      );
+      
+      expect(tooShortError[0]).toBeTruthy()
+      
+    })
+
+    it('should require length less than 16', async () => { 
+      const submitButton = await loader.getHarness(MatButtonHarness)
+      const compiled = fixture.debugElement.nativeElement;
+      const passInput = await Promise.all(
+        (await loader.getAllHarnesses(MatFormFieldHarness)).map(async (input) => {
+          const text = await input.getLabel();
+          return text === 'Password' ? input : null;
+        })
+      ).then(results => results.filter(input => input !== null)); // correctly working async filter
+
+      const control = await passInput[0].getControl() as unknown as MatInputHarness;
+      
+      await control.setValue('1@Aaaaaaaaaaaaaaaaaaaaa')
+
+      await submitButton.click()
+      fixture.detectChanges()
+
+      const errors = Array.from(compiled.querySelectorAll('mat-error'));
+      const tooLongError = (errors as HTMLElement[]).filter(
+        (error) => {
+          return error.textContent!.includes("Maximum 16 characters required")
+        }
+      );
+      
+      expect(tooLongError[0]).toBeTruthy()
+
+    })
+    
   })
 
   it('should require the passwords to match', () => { })
