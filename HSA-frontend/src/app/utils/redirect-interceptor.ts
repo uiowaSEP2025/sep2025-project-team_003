671ffdbb2632c3eb60f25147753bdabe2c28447a
Alex Guo <aguo2@uiowa.edu>
2025-04-12 23:47:23 -0500

    HSA-99: removed all errorhandler services, and made it an interceptor

---
+++ HSA-frontend/src/app/utils/redirect-interceptor.ts
@@ -0,0 +1,43 @@
+import { HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse, HttpResponse } from "@angular/common/http";
+import { Observable, catchError, tap, throwError } from "rxjs";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserAuthService } from "../services/user-auth.service";
+
+export function RedirectInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+    const router = inject(Router);
+    const userAuth = inject(UserAuthService)
+
+    return next(req).pipe(
+        tap(event => {
+            if (event instanceof HttpResponse) {
+                const status = event.status;
+                if (status >= 200 && status < 300) {
+                    // we are logged in
+                    userAuth.setLoginStatus(true)
+                    
+                } else if (status >= 300 && status < 400) {
+                    
+                }
+            }
+        }),
+
+        catchError((err: HttpErrorResponse) => {
+            const url = req.url;
+
+            console.error(`HTTP ${err.status}: ${err.message}`);
+
+            if (err.status === 401) {
+                router.navigateByUrl("/login");
+            } else if (err.status === 403) {
+                
+            } else if (err.status === 404) {
+                router.navigateByUrl("/404");
+            } else if (err.status >= 500) {
+                router.navigateByUrl("/500");
+            }
+
+            return throwError(() => err);
+        })
+    );
+}
