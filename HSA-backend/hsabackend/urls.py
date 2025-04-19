5b5537dcb6ebc1a1db32c565c732f0c8673451a1
Tan Nguyen <tannguyen@uiowa.edu>
2025-04-18 19:03:29 -0500

    HSA 121 add api excluded for job

--- HSA-backend/hsabackend/urls.py
+++ HSA-backend/hsabackend/urls.py
@@ -11,7 +11,7 @@ from hsabackend.views.requests import get_org_request_data, delete_request,appro
 from hsabackend.views.services import get_service_table_data, get_service_excluded_table_data, create_service, edit_service, delete_service
 from hsabackend.views.materials import get_material_excluded_table_data, get_material_table_data, create_material, edit_material, delete_material
 from hsabackend.views.invoices import createInvoice, getInvoices, deleteInvoice, updateInvoice, get_data_for_invoice
-from hsabackend.views.jobs import get_job_table_data, get_job_individual_data, create_job, edit_job, delete_job
+from hsabackend.views.jobs import get_job_excluded_table_data, get_job_table_data, get_job_individual_data, create_job, edit_job, delete_job
 from hsabackend.views.jobs_services import get_job_service_table_data, create_job_service, delete_job_service, delete_cached_job_service
 from hsabackend.views.jobs_materials import get_job_material_table_data, create_job_material, delete_job_material, delete_cached_job_material
 from hsabackend.views.jobs_contractors import get_job_contractor_table_data, create_job_contractor, delete_job_contractor, delete_cached_job_contractor
@@ -78,6 +78,7 @@ urlpatterns = [
 
     # jobs
     path("api/get/jobs", get_job_table_data),
+    path("api/get/jobs/exclude", get_job_excluded_table_data),
     path("api/get/job/<int:id>", get_job_individual_data),
     path("api/create/job", create_job),
     path("api/edit/job/<int:id>", edit_job),
