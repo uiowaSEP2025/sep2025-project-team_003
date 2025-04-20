1c080520fc15fd6a34830319542fa3ba3329c1d1
Alex Guo <aguo2@uiowa.edu>
2025-04-20 15:09:11 -0500

    HSA-121: made tests runnable

--- HSA-frontend/src/app/components/view-job-dialog-component/view-job-dialog-component.component.spec.ts
+++ HSA-frontend/src/app/components/view-job-dialog-component/view-job-dialog-component.component.spec.ts
@@ -1,16 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewJobDialogComponentComponent } from './view-job-dialog-component.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+// Mock MatDialogRef
+class MatDialogRefMock {
+  close() { }
+}
 
 describe('ViewJobDialogComponentComponent', () => {
   let component: ViewJobDialogComponentComponent;
   let fixture: ComponentFixture<ViewJobDialogComponentComponent>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ViewJobDialogComponentComponent]
+      imports: [
+        MatDialogModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useClass: MatDialogRefMock },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            "jobInfo": {
+              "data": {
+                "customerName": "John Doe",
+                "jobStatus": "In Progress",
+                "startDate": "2025-04-15",
+                "endDate": "2025-04-20",
+                "description": "Electrical work for residential home",
+                "requestorAddress": "123 Main St",
+                "requestorCity": "Springfield",
+                "requestorState": "IL",
+                "requestorZip": "62701",
+                "id": "JOB123456"
+              }
+            },
+            bookingInfo: { /* mock your bookingInfo data here */ }
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ViewJobDialogComponentComponent);
     component = fixture.componentInstance;
--- HSA-frontend/src/app/components/view-job-dialog-component/view-job-dialog-component.component.ts
+++ HSA-frontend/src/app/components/view-job-dialog-component/view-job-dialog-component.component.ts
@@ -10,8 +10,6 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { JobDataInterface } from '../../interfaces/api-responses/job.api.data.interface';
-import { JobService } from '../../services/job.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-job-dialog-component',
