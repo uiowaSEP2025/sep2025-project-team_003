696aa8f348c32da1d3831c538aea935383c9ac72
Stewart Sankey <ssankey@stewartsankey.com>
2025-04-23 22:34:41 -0500

    HSA-123: More optimizations

--- HSA-backend/hsabackend/views/contractors.py
+++ HSA-backend/hsabackend/views/contractors.py
@@ -1,13 +1,6 @@
-from django.core.exceptions import ValidationError
-from django.db.models import Q
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-from hsabackend.models.contractor import Contractor
-from hsabackend.serializers.contractor_serializer import ContractorSerializer
 from hsabackend.utils.auth_wrapper import check_authenticated_and_onboarded
-from utils.response_helpers import get_table_data
+from hsabackend.utils.response_helpers import get_table_data, delete_object, update_individual_data, create_individual_data
 
 
 @api_view(["GET"])
@@ -18,110 +11,17 @@ def get_contractor_table_data(request):
 @api_view(["GET"])
 @check_authenticated_and_onboarded(require_onboarding=False)
 def get_contractor_excluded_table_data(request):
-    org = request.org
-    search = request.query_params.get('search', '')
-    pagesize = request.query_params.get('pagesize', '')
-    offset = request.query_params.get('offset',0)
-    excluded_ids_str = request.GET.getlist('excludeIDs', [])
-    excluded_ids = [int(id) for id in excluded_ids_str]
-
-    if not pagesize or not offset:
-        return Response({"message": "missing pagesize or offset"}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
-        pagesize = int(pagesize)
-        offset = int(offset)
-    except:
-        return Response({"message": "pagesize and offset must be int"}, status=status.HTTP_400_BAD_REQUEST)
-    
-    offset = offset * pagesize
-    contractors = Contractor.objects.filter(organization=org.pk).exclude(id__in=excluded_ids).filter(
-        Q(first_name__icontains=search) | Q(last_name__icontains=search) 
-    )[offset:offset + pagesize] if search else Contractor.objects.filter(organization=org.pk).exclude(id__in=excluded_ids)[offset:offset + pagesize]
-
-    serializer = ContractorSerializer(contractors, many=True)
-    
-    count = Contractor.objects.filter(organization=org.pk).exclude(id__in=excluded_ids).filter(
-        Q(first_name__icontains=search) | Q(last_name__icontains=search)
-    ).count() if search else Contractor.objects.filter(organization=org.pk).exclude(id__in=excluded_ids).count()
-
-    res = {
-        'data': serializer.data,
-        'totalCount': count
-    }    
-    return Response(res, status=status.HTTP_200_OK)
+    return get_table_data(request, 'contractor', exclude=True)
     
 @api_view(["POST"])
 @check_authenticated_and_onboarded(require_onboarding=False)
 def create_contractor(request):
-    org = request.org
-
-    first_name = request.data.get('firstName', '')
-    last_name = request.data.get('lastName', '')
-    email = request.data.get('email', '')
-    phone = request.data.get('phone', '').replace("-","")
-
-    contractor_data = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "phone": phone,
-        "organization": org,
-    }
-
-    serializer = ContractorSerializer(data=contractor_data)
-
-    try:
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"message": "Contractor created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
-    except ValidationError as e:
-        return Response({"errors": e.message_dict}, status=status.HTTP_400_BAD_REQUEST)
-
+    return create_individual_data(request, "contractor")
 @api_view(["POST"])
 @check_authenticated_and_onboarded()
-def edit_contractor(request, id):
-    org = request.org
-    contractor_qs = Contractor.objects.filter(pk=id, organization=org)
-
-    if not contractor_qs.exists():
-        return Response({"message": "The contractor does not exist"}, status=status.HTTP_404_NOT_FOUND)
-    
-    contractor = contractor_qs[0]
-
-    org = request.org
-
-    first_name = request.data.get('firstName', '')
-    last_name = request.data.get('lastName', '')
-    email = request.data.get('email', '')
-    phone = request.data.get('phone', '').replace("-", "")
-
-    contractor_data = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "phone": phone,
-        "organization": org,
-    }
-
-    serializer = ContractorSerializer(data=contractor_data)
-    
-    try:
-        serializer.is_valid(raise_exception=True)  # Validate the model instance
-        serializer.update(contractor, contractor_data)
-        return Response({"message": "Contractor edited successfully"}, status=status.HTTP_200_OK)
-    except ValidationError as e:
-        return Response({"errors": e.message_dict}, status=status.HTTP_400_BAD_REQUEST)
-
+def edit_contractor(request, contractor_id):
+    return update_individual_data(request, contractor_id, "contractor")
 @api_view(["POST"])
 @check_authenticated_and_onboarded(require_onboarding=False)
-def delete_contractor(request, id):
-    org = request.org
-    cust = Contractor.objects.filter(pk=id, organization=org)
-
-    if not cust.exists():
-        return Response({"message": "The contractor does not exist"}, status=status.HTTP_404_NOT_FOUND)
-    
-    cust[0].delete()
-
-    return Response({"message": "Contractor deleted successfully"}, status=status.HTTP_200_OK)
+def delete_contractor(request, contractor_id):
+    return delete_object(request, contractor_id, "contractor")
