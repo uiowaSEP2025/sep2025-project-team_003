0c729b710c240a4517d7867d1ad7f5c9980d36b5
Stewart Sankey <stewart-sankey@uiowa.edu>
2025-05-02 17:06:44 -0500

    HSA-118: Updating services JSON to match model.

--- HSA-frontend/src/app/interfaces/api-responses/jobTemplate.api.display.interface.ts
+++ HSA-frontend/src/app/interfaces/api-responses/jobTemplate.api.display.interface.ts
@@ -5,9 +5,9 @@ export interface JobTemplateDisplayInterface {
 }
 
 export interface ServiceJSON{
-    "serviceID": string,
-    "serviceName": string,
-    "serviceDescription": string,
+    "id": string,
+    "name": string,
+    "description": string,
     "fee": number
 }
 
