c83b548ef80e5c57f6b34e8a534d2cb131ac1474
Alex Guo <aguo2@uiowa.edu>
2025-05-04 11:20:57 -0500

    HSA-135: added contractor tessts

--- HSA-backend/hsabackend/tests/test_contractors.py
+++ HSA-backend/hsabackend/tests/test_contractors.py
@@ -1,206 +1,295 @@
-from unittest.mock import MagicMock, Mock, patch
-from rest_framework.test import APIRequestFactory
-from django.contrib.auth.models import User
-from hsabackend.models.organization import Organization
 from rest_framework.test import APITestCase
-from rest_framework import status
-from hsabackend.views.quotes import getQuotesForInvoiceByCustomer, getQuotesForInvoiceByInvoice
+from unittest.mock import patch, Mock, MagicMock
+from rest_framework.test import APIRequestFactory
+from hsabackend.views.contractors import get_all_contractors_for_org, get_contractor_table_data, get_contractor_excluded_table_data, create_contractor, edit_contractor, delete_contractor
+from hsabackend.models.organization import Organization
+from django.core.exceptions import ValidationError
 
-class testQuotesView(APITestCase):
-    def test_get_quotes_by_cust_id_unauth(self):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = False
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/customer/1?search&pagesize=100&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByCustomer(request,1)
-        
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_cust_id_missing_pagesize(self, org):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-        
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        factory = APIRequestFactory()
-        
-        request = factory.get('api/get/quotesforinvoice/customer/1?search&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByCustomer(request,1)
-        
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_cust_id_invalid_page_size(self, org):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-        
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        factory = APIRequestFactory()
-        
-        request = factory.get('api/get/quotesforinvoice/customer/1?search&pagesize=a&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByCustomer(request,1)
-        
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    @patch('hsabackend.views.quotes.Customer.objects.filter')
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_cust_id_cust_not_found(self, org, cust):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-        
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        factory = APIRequestFactory()
-
-        custqs = Mock()
-        custqs.exists.return_value = False
-        cust.return_value = custqs
-        
-        request = factory.get('api/get/quotesforinvoice/customer/1?search&pagesize=10&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByCustomer(request,1)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
-
-    @patch('hsabackend.views.quotes.Quote.objects.filter')
-    @patch('hsabackend.views.quotes.Quote.objects.select_related')
-    @patch('hsabackend.views.quotes.Customer.objects.filter')
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_cust_id_ok(self, org, cust, quote, quote_count):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-        
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        factory = APIRequestFactory()
-
-        custqs = Mock()
-        custqs.exists.return_value = True
-        cust.return_value = custqs
-        
-        quoteqs1 = Mock(name='q1')
-        quote.return_value = quoteqs1
-
-        quoteqs2 = Mock(name='q2')
-        quoteqs1.select_related.return_value = quoteqs2
-
-        filtered_quotes = MagicMock(name='magic')
-        quoteqs2.filter.return_value = filtered_quotes
-
-        quote_count.return_value = Mock()
-
-        request = factory.get('api/get/quotesforinvoice/customer/1?search&pagesize=10&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByCustomer(request,1)
-        assert response.status_code == status.HTTP_200_OK
-
-    def test_get_quotes_by_invoice_unauth(self):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = False
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/invoice/1?search&pagesize=100&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByInvoice(request,1)
-        
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_invoice_no_page_size(self, org):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/invoice/1?search&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByInvoice(request,1)
-
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_invoice_invalid_page_size(self, org):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/invoice/1?search&pagesize=s&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByInvoice(request,1)
-
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    @patch('hsabackend.views.quotes.Invoice.objects.filter')
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_invoice_invoice_not_found(self, org, invoice):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-
-        invoice_qs = Mock()
-        invoice_qs.exists.return_value = False
-        invoice.return_value = invoice_qs
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/invoice/1?search&pagesize=10&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByInvoice(request,1)
-
-        assert response.status_code == status.HTTP_404_NOT_FOUND
-
-    @patch('hsabackend.views.quotes.Quote.objects.filter')
-    @patch('hsabackend.views.quotes.Quote.objects.select_related')
-    @patch('hsabackend.views.quotes.Invoice.objects.filter')
-    @patch('hsabackend.views.quotes.Organization.objects.get')
-    def test_get_quotes_by_invoice_ok(self, org, invoice, quote, quote_count):
-        mock_user = Mock(spec=User)
-        mock_user.is_authenticated = True
-
-        organization = Organization()
-        organization.is_onboarding = False
-        org.return_value = organization
-
-        invoice_qs = MagicMock()
-        invoice_qs.exists.return_value = True
-        invoice.return_value = invoice_qs
-
-        mock_cust = MagicMock()
-        invoice_qs.__getitem__.side_effect = lambda x: mock_cust
-
-        quoteqs1 = Mock(name='q1')
-        quote.return_value = quoteqs1
-
-        quoteqs2 = Mock(name='q2')
-        quoteqs1.select_related.return_value = quoteqs2
-
-        filtered_quotes = MagicMock(name='magic')
-        quoteqs2.filter.return_value = filtered_quotes
-
-        quote_count.return_value = Mock()
-        
-        factory = APIRequestFactory()
-        request = factory.get('api/get/quotesforinvoice/invoice/1?search&pagesize=10&offset=0')
-        request.user = mock_user  
-        response = getQuotesForInvoiceByInvoice(request,1)
-
-        assert response.status_code == status.HTTP_200_OK
+class TestGetAllContractors(APITestCase):
     
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_contractors_ok(self, org, mock):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/all/contractors')
+        request.org = mock_org
+        request.user = mock_user
+
+        res = get_all_contractors_for_org(request)
+        assert res.status_code == 200
+        mock.assert_called_once()
+
+class TestGetContractorTableData(APITestCase):
+
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_get_contractor_table_no_offset(self, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_table_data(request)
+        assert res.status_code == 400
+
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_get_contractor_table_offset_not_int(self, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors?pagesize=10&offset=a')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_table_data(request)
+        assert res.status_code == 400
+
+    
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    def test_get_contractor_table_ok(self, filter, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+
+        mock_qs = MagicMock()
+        filter.return_value = mock_qs
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors?pagesize=10&offset=2')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_table_data(request)
+        assert res.status_code == 200
+
+class TestGetContractorExcluded(APITestCase):
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_get_contractor_table_no_offset(self, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors/exclude')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_excluded_table_data(request)
+        assert res.status_code == 400
+
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_get_contractor_table_offset_not_int(self, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors/exclude?pagesize=10&offset=a')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_excluded_table_data(request)
+        assert res.status_code == 400
+
+    
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    def test_get_contractor_table_ok(self, filter, org):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+
+        mock_qs = MagicMock()
+        filter.return_value = mock_qs
+        
+        factory = APIRequestFactory()
+        request = factory.get('api/get/contractors/exclude?pagesize=10&offset=2')
+        request.org = mock_org
+        request.user = mock_user
+        res = get_contractor_excluded_table_data(request)
+        assert res.status_code == 200
+
+class TestCreateContractor(APITestCase):
+    @patch('hsabackend.views.contractors.Contractor')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_create_invalid_data(self,org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        con.return_value = mock
+        mock.full_clean.side_effect = ValidationError({'name': ['Error while saving organization']})
+
+        factory = APIRequestFactory()
+        request = factory.post('api/create/contractor')
+        request.org = mock_org
+        request.user = mock_user
+        res = create_contractor(request)
+        assert res.status_code == 400
+
+    @patch('hsabackend.views.contractors.Contractor')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_create_ok(self,org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        con.return_value = mock
+
+        factory = APIRequestFactory()
+        request = factory.post('api/create/contractor')
+        request.org = mock_org
+        request.user = mock_user
+        res = create_contractor(request)
+        mock.save.assert_called_once()
+        assert res.status_code == 201
+
+class TestEditContractor(APITestCase):
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_edit_not_found(self, org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        mock.exists.return_value = False
+        con.return_value = mock
+
+        factory = APIRequestFactory()
+        request = factory.post('api/edit/contractor')
+        request.user = mock_user
+
+        res = edit_contractor(request, 2)
+
+        assert res.status_code == 404
+
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_edit_not_valid(self, org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        mock.exists.return_value = True
+        con.return_value = mock
+
+        mock_cust = Mock(name="MockCust")
+        mock.__getitem__.side_effect = lambda x: mock_cust
+
+        mock_cust.full_clean.side_effect = ValidationError({'name': ['Error while saving organization']})
+
+        factory = APIRequestFactory()
+        request = factory.post('api/edit/contractor')
+        request.user = mock_user
+
+        res = edit_contractor(request, 2)
+        assert res.status_code == 400
+
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_edit_ok(self, org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        mock.exists.return_value = True
+        con.return_value = mock
+
+        mock_cust = Mock(name="MockCust")
+        mock.__getitem__.side_effect = lambda x: mock_cust
+
+        factory = APIRequestFactory()
+        request = factory.post('api/edit/contractor')
+        request.user = mock_user
+
+        res = edit_contractor(request, 2)
+        assert res.status_code == 200
+
+class TestDelete(APITestCase):
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_delete_not_found(self, org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        mock.exists.return_value = False
+        con.return_value = mock
+
+        factory = APIRequestFactory()
+        request = factory.post('api/delete/contractor/2')
+        request.user = mock_user
+
+        res = delete_contractor(request, 2)
+        assert res.status_code == 404
+
+    @patch('hsabackend.views.contractors.Contractor.objects.filter')
+    @patch('hsabackend.utils.auth_wrapper.Organization.objects.get')
+    def test_delete_ok(self, org, con):
+        mock_user = Mock()
+        mock_user.is_authenticated = True
+
+        mock_org = Organization()
+        mock_org.is_onboarding = False
+        org.return_value = mock_org
+                
+        mock = MagicMock()
+        mock.exists.return_value = True
+        con.return_value = mock
+
+        factory = APIRequestFactory()
+        request = factory.post('api/delete/contractor/2')
+        request.user = mock_user
+
+        res = delete_contractor(request, 2)
+        assert res.status_code == 200
