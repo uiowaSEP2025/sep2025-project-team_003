497ac8176a073b7d22da25869e431278e2c6a307
Mike Zeng <mzen17.jp@gmail.com>
2025-05-06 16:03:41 +0000

    utilize JWT instead of random pins

--- HSA-backend/hsabackend/models/job.py
+++ HSA-backend/hsabackend/models/job.py
@@ -31,7 +31,6 @@ class Job(models.Model):
     ]
 
     quote_s3_link = models.CharField(max_length=100, blank=True, null=True)
-    quote_sign_pin = models.CharField(max_length=10, blank=True, null=True)
     quote_status = models.CharField(max_length=50, choices=quote_choices, default="not-created-yet")
 
     def __str__(self):
