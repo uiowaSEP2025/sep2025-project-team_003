2839d6b2dfa5e9757321a86d75e9ef89b20864e7
Stewart Sankey <ssankey@stewartsankey.com>
2025-05-07 21:43:41 -0500

    test

--- deployment/testing/frontend/entrypoint.sh
+++ deployment/testing/frontend/entrypoint.sh
@@ -4,6 +4,3 @@ set -x
 
 # Run tests in headless mode
 ng test --watch=false --browsers=ChromeHeadless --code-coverage
-
-# Optionally, create a summary of the coverage for easier parsing
-npx istanbul report text-summary
